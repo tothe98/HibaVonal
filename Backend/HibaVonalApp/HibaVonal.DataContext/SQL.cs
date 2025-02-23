dab2287ad4bea206192f67ee3431e956f9dc0b8d
tothe98 <totherikjanos@gmail.com>
2025-02-23 01:16:24 +0100

    Backend init and database connection config

---
+++ Backend/HibaVonalApp/HibaVonal.DataContext/SQL.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HibaVonal.DataContext
+{
+    public class SQL: DbContext
+    {
+        public SQL(DbContextOptions options) : base(options) { }
+    }
+
+    //Ide jönnek a DbSet-ek
+
+}
