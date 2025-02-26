a28755266da0486be63db1ea1e0d5526787b1850
Nazenano <113120126+Nazenano@users.noreply.github.com>
2025-02-26 21:56:10 +0100

    Initialized nextjs app for frontend

---
+++ Frontend/hibavonalapp/tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "tailwindcss"
+
+export default {
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+
+                'err-critical': "#6C0000",
+                'err-high': "#DC4465",
+                'err-medium': "#FE860E",
+                'err-low': "#FECC60",
+
+            },
+        },
+    },
+    plugins: [],
+} satisfies Config
